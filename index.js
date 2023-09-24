c362f493295cca616fa41ed0306922ecc8e10e21
nishida-sugiura <108458541+nishida-sugiura@users.noreply.github.com>
2023-09-24 14:56:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,33 @@ $(function () {
         return false;
         
     });
-});
+}
+ 
+ 
+
+
+/* 
+   * liff.sendMessages()
+   * Botにメッセージを送信する
+   */
+  document.getElementById('sendstatusbutton').addEventListener('click', function () {
+    liff.sendMessages([
+      {
+        type: 'text',
+        text: "今のポイント"
+      }
+    ]).then(function () {
+      window.alert("Message sent");
+    }).catch(function (error) {
+      window.alert("Error sending message: " + error);
+    });
+  });
+
+
+  
+ 
+ 
+ );
 
 
 
